98c72d3b65198df932a4bf6de54ab8833ca64c44
rusherimfa <minaseimfa@gmail.com>
2026-05-03 21:13:00 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -80,16 +80,8 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        // Default Menus
-        $menus = [
-            ['name' => 'Wagyu A5 Ribeye', 'category' => 'Main Course', 'price' => 750000, 'description' => 'Steak premium dari sapi pilihan Jepang.', 'is_available' => true],
-            ['name' => 'Truffle Mushroom Soup', 'category' => 'Appetizer', 'price' => 85000, 'description' => 'Sup krim jamur gurih dengan tetesan minyak truffle murni.', 'is_available' => true],
-            ['name' => 'Artisan Lychee Tea', 'category' => 'Beverage', 'price' => 45000, 'description' => 'Teh kraf dengan buah leci segar utuh.', 'is_available' => true],
-        ];
-
-        foreach ($menus as $menu) {
-            Menu::firstOrCreate(['name' => $menu['name']], array_merge($menu, ['team_id' => $team->id]));
-        }
+        // Default Menus from OceansRestoMenuSeeder
+        $this->call(OceansRestoMenuSeeder::class);
 
         // Floor Plan Tables
         $tables = [
